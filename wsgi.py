13792aead64e63296c3fd28af2a262417f27e7a4
Enes Yigit <yig210491@spengergasse.at>
2026-01-10 04:39:54 +0100

    Programm v08 Render

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,3 @@
-# wsgi.py (Root of the repository)
-# This file serves as the entry point for WSGI servers like Gunicorn
+from Skinlytics.backend.app import create_app
 
-# Import the application instance from the backend module
-from Skinlytics.backend.wsgi import app  # This is the Flask app instance
-
-# The WSGI application object is used by the server to load the application
-# This is the standard variable name expected by WSGI servers
-application = app
+app = create_app()
